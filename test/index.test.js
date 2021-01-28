ad15ae29e7471ec1b306e4777795074de2127662
sigalor <nalenz@protonmail.com>
2021-01-28 12:35:31 +0100

    Add unit test case for simple failure

--- test/index.test.js
+++ test/index.test.js
@@ -19,6 +19,12 @@ test('countPDFPages', async () => {
   await expect(gs.countPDFPages(pdfFiles[1])).resolves.toBe(3);
 });
 
+test('countPDFPages fails for invalid PDF', async () => {
+  await expect(gs.countPDFPages(Buffer.from([1, 2, 3]))).rejects.toThrow(
+    /^Failed to determine number of pages in PDF: Command failed/,
+  );
+});
+
 test('extractPDFPages', async () => {
   const res = await gs.extractPDFPages(pdfFiles[1], 2, 3);
   await expect(gs.countPDFPages(res)).resolves.toBe(2);
