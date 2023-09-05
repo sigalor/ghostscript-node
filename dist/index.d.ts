7f682087f38c95733f28accb9cbaebdb9fbe947c
Mohamed Aziz Khayati <mohamed.aziz.khayati@divizend.com>
2023-09-05 17:35:39 +0200

    chore: run build with new changes

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -4,6 +4,17 @@ export declare function countPDFPages(pdfBuffer: Buffer): Promise<number>;
 export declare function extractPDFPages(pdfBuffer: Buffer, firstPage: number, lastPage: number): Promise<Buffer>;
 export declare function rotatePDF(pdfBuffer: Buffer, direction: '90' | '180' | '270'): Promise<Buffer>;
 /**
+ * Converts a PDF to PDF/A.
+ *
+ * @param pdfBuffer - Buffer of the PDF to convert
+ * @param options - Options for the conversion
+ * @param options.version - PDF/A version to convert to. Defaults to 1.
+ * @returns
+ */
+export declare function convertToPDFA(pdfBuffer: Buffer, options?: {
+    version: number;
+}): Promise<Buffer>;
+/**
  * If `firstPage` is not given, 1 is used.
  * If `lastPage` is not given, the document's last page is used.
  * If `firstPage` is negative (e.g. -n), this refers to the last n pages and `lastPage` must be undefined.
